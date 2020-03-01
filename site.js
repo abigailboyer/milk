6ebd7a79c39797382b65658fba79ccf2d3230868
abigail <abigailboyer1@gmail.com>
2020-03-01 15:12:11 -0600

    fix add and subtract buttons so that they dont return NaN anymore

--- site.js
+++ site.js
@@ -1,37 +1,58 @@
 //calculate without jquery
-var form = document.getElementById("milk");
-form.addEventListener('submit', milk);
+var form = document.getElementById("submit");
+form.addEventListener('click', milk);
 
-function milk(event){
-  event.preventDefault();
+function milk(e){
+  e.preventDefault();
   var milk = parseInt(document.querySelector("#milkInput").value);
 
   if (milk <= 0) {
     console.log("Good");
-    document.querySelector("#results").innerHTML = "good.";
+    good();
   } else if (milk > 0) {
     console.log("Bad");
-    document.querySelector("#results").innerHTML = "bad. you're disgusting milk is gross";
+    bad();
   } else {
     console.log("error");
-    document.querySelector("#results").innerHTML = "something went wrong :/";
+    error();
   }
 }
 
+function good(){
+  $('#results').text("good. milk is disgusting");
+}
+
+function bad(){
+  $('#results').text("that's disgusting. milk is gross and so are you");
+}
+
+function error(){
+  $('#results').text("try entering a number");
+}
+
 //add easier buttons with jquery
+//error check to prevent NaN when using little buttons to add and sub
+
 $('#milkInput').after('<a id="more" href="#null">+</a>')
 $('#milkInput').before('<a id="less" href="#null">-</a>')
 
 $('#more').on('click', function(e) {
   var milkInput = $('#milkInput').val();
-  var newValue = parseInt(milkInput) + 1;
-  $('#milkInput').val(newValue);
+  if(isNaN(milkInput)){
+    $('#results').text("is that a number?");
+  } else {
+    var newValue = parseInt(milkInput) + 1;
+    $('#milkInput').val(newValue);
+  }
+  e.preventDefault();
 })
 
 $('#less').on('click', function(e) {
   var milkInput = $('#milkInput').val();
-  var newValue = parseInt(milkInput) - 1;
-  $('#milkInput').val(newValue);
+  if(isNaN(milkInput)){
+    $('#results').text("is that a number?");
+  } else {
+    var newValue = parseInt(milkInput) - 1;
+    $('#milkInput').val(newValue);
+  }
 })
-
-//error check to prevent NaN when using little buttons to add and sub
